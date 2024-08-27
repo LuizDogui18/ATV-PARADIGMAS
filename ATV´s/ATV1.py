818bf9347bf8690849d5da8f32eb45eb88c8bf63
LuizCarlos18 <113647666+LuizDogui18@users.noreply.github.com>
2024-08-26 22:55:57 -0300

    Add files via upload

---
+++ ATV´s/ATV1.py
@@ -0,0 +1,4 @@
+numero = int(input("Digite um numero: "))
+
+for numero in range(1, numero + 1):
+    print("*" * numero)
---
+++ ATV´s/ATV2.py
@@ -0,0 +1,4 @@
+numero = int(input("Digite um número: "))
+
+for numero in range(numero, 0, -1):
+    print("*" * numero)
